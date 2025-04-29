5a4dbaaafb4c440fad5a260eab503fefd3f06cf7
Christina Chortaria <actspatial@gmail.com>
2025-04-29 12:24:40 -0400

    Add ruby interface for isbn validation
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- ext/library_standard_numbers/src/lib.rs
+++ ext/library_standard_numbers/src/lib.rs
@@ -1,9 +1,14 @@
 use magnus::{function, prelude::*, Error, Ruby};
 use library_stdnums::lccn::LCCN;
+use library_stdnums::isbn::ISBN;
 use library_stdnums::traits::Valid;
 
 fn lccn_validate(identifier: String) -> bool {
     LCCN::new(identifier).valid()
+}
+
+fn isbn_validate(identifier: String) -> bool {
+    ISBN::new(identifier).valid()
 }
 
 #[magnus::init]
@@ -11,5 +16,8 @@ fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("LibraryStandardNumbers")?;
     let lccn_module = module.define_module("LCCN")?;
     lccn_module.define_singleton_method("valid?", function!(lccn_validate, 1))?;
+
+    let isbn_module = module.define_module("ISBN")?;
+    isbn_module.define_singleton_method("valid?", function!(isbn_validate, 1))?;
     Ok(())
 }
