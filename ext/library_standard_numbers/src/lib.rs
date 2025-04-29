354095589b977d6a35a4764546f380b1e2156e51
Jane Sandberg <js7389@princeton.edu>
2025-04-29 12:14:59 -0700

    Add ruby interfaces for ISSN methods
    Also, refactor the tests into their own files, to avoid a
    rubocop error.

--- ext/library_standard_numbers/src/lib.rs
+++ ext/library_standard_numbers/src/lib.rs
@@ -1,6 +1,7 @@
 use magnus::{function, prelude::*, Error, RModule, Ruby};
 use library_stdnums::lccn::LCCN;
 use library_stdnums::isbn::ISBN;
+use library_stdnums::issn::ISSN;
 use library_stdnums::traits::{Normalize, Valid};
 
 fn lccn_validate(identifier: String) -> bool {
@@ -18,11 +19,21 @@ fn isbn_validate(identifier: String) -> bool {
 fn isbn_normalize(identifier: String) -> Option<String> {
     ISBN::new(identifier).normalize()
 }
+
+fn issn_validate(identifier: String) -> bool {
+    ISSN::new(identifier).valid()
+}
+
+fn issn_normalize(identifier: String) -> Option<String> {
+    ISSN::new(identifier).normalize()
+}
+
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("LibraryStandardNumbers")?;
     create_lccn_module(&module)?;
-    create_isbn_module(&module)?;  
+    create_isbn_module(&module)?;
+    create_issn_module(&module)?;
 
     Ok(())
 }
@@ -39,6 +50,14 @@ fn create_isbn_module(module: &RModule) -> Result<(), Error> {
     let isbn_module = module.define_module("ISBN")?;
     isbn_module.define_singleton_method("valid?", function!(isbn_validate, 1))?;
     isbn_module.define_singleton_method("normalize", function!(isbn_normalize, 1))?;
+
+    Ok(())
+}
+
+fn create_issn_module(module: &RModule) -> Result<(), Error> {
+    let isbn_module = module.define_module("ISSN")?;
+    isbn_module.define_singleton_method("valid?", function!(issn_validate, 1))?;
+    isbn_module.define_singleton_method("normalize", function!(issn_normalize, 1))?;
 
     Ok(())
 }
--- spec/library_standard_numbers_spec.rb
+++ spec/library_standard_numbers_spec.rb
@@ -4,24 +4,4 @@ RSpec.describe LibraryStandardNumbers do
   it "has a version number" do
     expect(LibraryStandardNumbers::VERSION).not_to be nil
   end
-
-  it "validates LCCNs" do
-    expect(LibraryStandardNumbers::LCCN.valid?("n78-890351")).to be true
-    expect(LibraryStandardNumbers::LCCN.valid?("n78-89035100444")).to be false
-  end
-  it "normalizes LCCNs" do
-    expect(LibraryStandardNumbers::LCCN.normalize("n  78890351 ")).to eq "n78890351"
-  end
-  it "returns nil when attempting to normalize an invalid LCCN" do
-    expect(LibraryStandardNumbers::LCCN.normalize("na078-890351")).to be_nil
-  end
-  it "normalizes ISBNs" do
-    expect(LibraryStandardNumbers::ISBN.normalize("0-306-40615-2")).to eq "9780306406157"
-  end
-  it "returns nil when attempting to normalize an invalid ISBN" do
-    expect(LibraryStandardNumbers::ISBN.normalize("na078-890351")).to be_nil
-  end
-  it "validates ISBNs" do
-    expect(LibraryStandardNumbers::ISBN.valid?("9781449373320")).to be true
-  end
 end
